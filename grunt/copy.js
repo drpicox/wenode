1489529ba979d1ae1730fd4f7d9f223b6e658ae7
Patrick Heneise <patrick.heneise@gmail.com>
2014-08-24 17:07:56 +0200

    added cname

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,25 +1,29 @@
 module.exports = {
 	options: {
-		onlyIf: 'modified',
+		onlyIf: 'modified'
 	},
 	css: {
 		expand: true,
 		cwd: '<%= config.src %>',
 		dest: '<%= config.dist %>',
-		src: '**/*.css',
+		src: '**/*.css'
 	},
 	favicon: {
 		src: '<%= config.src %>/favicon.ico',
-		dest: '<%= config.dist %>/favicon.ico',
+		dest: '<%= config.dist %>/favicon.ico'
 	},
 	fonts: {
 		src: 'bower_components/*/fonts/*',
-		dest: '<%= config.dist %>/',
+		dest: '<%= config.dist %>/'
 	},
 	svg: {
 		expand: true,
 		cwd: '<%= config.src %>',
 		dest: '<%= config.dist %>',
-		src: '**/*.svg',
+		src: '**/*.svg'
 	},
+	cname: {
+		src: '<%= config.src %>/CNAME',
+		dest: '<%= config.dist %>/CNAME'
+	}
 };
